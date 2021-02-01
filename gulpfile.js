368267fd5d7cd1acf1e9a6cc6920fb334d9cabe7
Rekryt <rekrytkw@gmail.com>
2021-02-01 23:41:52 +0300

    pretty fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 [
-	"./gulp/tasks/styles.js",
-	"./gulp/tasks/inky.js",
-	"./gulp/tasks/browserSync.js",
-	"./gulp/tasks/watch.js",
-	"./gulp/tasks/default.js",
+	'./gulp/tasks/styles.js',
+	'./gulp/tasks/inky.js',
+	'./gulp/tasks/browserSync.js',
+	'./gulp/tasks/watch.js',
+	'./gulp/tasks/default.js',
 ].forEach(function (taskPath) {
 	require(taskPath)();
 });
