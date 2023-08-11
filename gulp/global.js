9b8ab2000738e622303422a731ad53923c949606
Rekryt <rekrytkw@gmail.com>
2023-08-11 14:29:31 +0300

    Dependency updates

--- gulp/global.js
+++ gulp/global.js
@@ -1,20 +1,18 @@
 const $ = {
-	/* config */
-	config: require('./config.js')(),
+    /* config */
+    config: require('./config.js')(),
 
-	/* gulp modules */
-	gulp: require('gulp'),
-	gulpInlineCSS: require('gulp-inline-css'),
-	gulpInlineSource: require('gulp-inline-source'),
-	gulpSASS: require('gulp-sass'),
+    /* gulp modules */
+    gulp: require('gulp'),
+    gulpInlineCSS: require('gulp-inline-css'),
+    gulpInlineSource: require('gulp-inline-source'),
+    gulpSASS: require('gulp-sass')(require('sass')),
 
-	/* inky */
-	inky: require('inky'),
+    /* inky */
+    inky: require('inky'),
 
-	/* browser-sync */
-	browserSync: require('browser-sync'),
+    /* browser-sync */
+    browserSync: require('browser-sync'),
 };
-
-$.gulpSASS.compiler = require('node-sass');
 
 module.exports = $;
